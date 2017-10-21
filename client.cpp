6430f968bb2838e4e8c18345243710c4ebe09eaf
jarvisru <dar58896123@gmail.com>
2017-10-21 18:32:58 +0800

    successfully paint on both screen

--- client.cpp
+++ client.cpp
@@ -1,9 +1,58 @@
 #pragma comment(lib, "Ws2_32.lib")
 #include<WinSock2.h>
-
+#include<windows.h>
+#include <cstdlib>
 #include<assert.h>
 #include<iostream>
+#include<string>
 using namespace std;
+
+
+int board[15][29]= {
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {5,0,3,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,4,0,5},
+        {5,0,0,0,3,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,4,0,0,0,5},
+        {5,0,0,0,0,0,3,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,4,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,3,0,0,0,0,0,5,0,0,0,0,0,4,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,3,0,0,0,5,0,0,0,4,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,0,0,3,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,5},
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {5,0,0,0,0,0,0,0,0,0,0,0,4,0,5,0,3,0,0,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,4,0,0,0,5,0,0,0,3,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,4,0,0,0,0,0,5,0,0,0,0,0,3,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,4,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,3,0,0,0,0,0,5},
+        {5,0,0,0,4,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,3,0,0,0,5},
+        {5,0,4,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,3,0,5},
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+void paint(){
+    for(int row=0; row<15 ; row++){
+        for(int col=0; col<29 ; col++){
+            switch(board[row][col]){
+                case 0:
+                    cout<<" ";
+                    break;
+                case 1:
+                    cout<<"◇";
+                    break;
+                case 2:
+                    cout<<"—";
+                    break;
+                case 3:
+                    cout<<"＼";
+                    break;
+                case 4:
+                    cout<<"／";
+                    break;
+                case 5:
+                    cout<<"︱";
+                    break;
+            }
+        }
+        cout<<endl;
+    }
+}
+
 
 int main(){
     WSADATA wsaData;
@@ -23,19 +72,55 @@ int main(){
 
     // connect : int connect (SOCKET s, const struct sockaddr* addr, int addrlen)
     if(connect(client_socket,(SOCKADDR*)&addr, sizeof(addr))<0){
-        cout<<"client connection error ! ";
+        cout<<"連線失敗!請確認server端已開啟 ";
+    }
+    else{
+        char message[100];
+        ZeroMemory(message, 100);
+        recv(client_socket,message,sizeof(message),0);
+        cout<<message<<endl;
+
+        //規則說明
+        cout<<"【三子棋】"<<endl;
+        cout<<"1. 棋盤如下"<<endl;
+        cout<<"2. 雙方各執三子，依序放入棋盤中，三子全下完後，才可移動"<<endl;
+        cout<<"3. 移動方式可直走，橫走，也可對角線走，先三子連成一線者獲勝（直線or斜線皆可）"<<endl;
+        paint();
+        cout<<"開始遊戲 ? (1)開始 (0)結束 :";
+        int start=0;
+        int over = 0;
+        char *sendbuf;
+        while(cin>>start){
+           // start the game
+            if(start==1){
+                sendbuf="玩家進入";
+                send(client_socket, sendbuf, (int)strlen(sendbuf), 0);
+                Sleep(1000);
+                system("cls");
+                paint();
+            }
+            // end the game
+            else if(start==0){
+                closesocket(client_socket);
+                return 0;
+            }
+            // if input wrong
+            else{
+                cout<<"請輸入 數字1(開始) 或 數字0(結束):";
+            }
+        }
     }
     // send : int send (SOCKET s, const char* buffer, int len, int flags)
-    // char *sendbuf = "sending test!";
-    // send(client_socket, sendbuf, sizeof(sendbuf), 0);
+//     char *sendbuf = "sending test!";
+//     send(client_socket, sendbuf, sizeof(sendbuf), 0);
 
     // recv : int recv (SOCKET s, char *buf, int len, int flags)
-    char message[100];
-    ZeroMemory(message, 100);
-    recv(client_socket,message,sizeof(message),0);
-    cout<<message<<endl;
 
-    closesocket(client_socket);
+
+
+
+
+    // closesocket(client_socket);
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,7 +1,56 @@
-#include <WinSock2.h>
-#include <assert.h>
-#include <iostream>
+#pragma comment(lib, "Ws2_32.lib")
+#include<WinSock2.h>
+#include<windows.h>
+#include <cstdlib>
+#include<assert.h>
+#include<iostream>
+#include<string>
 using namespace std;
+
+int board[15][29]= {
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {5,0,3,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,4,0,5},
+        {5,0,0,0,3,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,4,0,0,0,5},
+        {5,0,0,0,0,0,3,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,4,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,3,0,0,0,0,0,5,0,0,0,0,0,4,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,3,0,0,0,5,0,0,0,4,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,0,0,3,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,5},
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {5,0,0,0,0,0,0,0,0,0,0,0,4,0,5,0,3,0,0,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,0,0,4,0,0,0,5,0,0,0,3,0,0,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,0,0,4,0,0,0,0,0,5,0,0,0,0,0,3,0,0,0,0,0,0,0,5},
+        {5,0,0,0,0,0,4,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,3,0,0,0,0,0,5},
+        {5,0,0,0,4,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,3,0,0,0,5},
+        {5,0,4,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,3,0,5},
+        {1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+void paint(){
+    for(int row=0; row<15 ; row++){
+        for(int col=0; col<29 ; col++){
+            switch(board[row][col]){
+                case 0:
+                    cout<<" ";
+                    break;
+                case 1:
+                    cout<<"◇";
+                    break;
+                case 2:
+                    cout<<"—";
+                    break;
+                case 3:
+                    cout<<"＼";
+                    break;
+                case 4:
+                    cout<<"／";
+                    break;
+                case 5:
+                    cout<<"︱";
+                    break;
+            }
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     WSADATA wsaData;
@@ -32,15 +81,33 @@ int main(){
     // listen : int listen (SOCKET s, int backlog);
     // if success -> return 0
     int l = listen(server_socket,SOMAXCONN);
+    assert(l>=0);
+    cout<<"等待玩家連線.."<<endl;
 
     // accept : SOCKET accept (SOCKET s, struct sockaddr *addr, int *addrlen)
-
     SOCKET sConnect;
     SOCKADDR_IN client_Addr; // client address information
     int client_Addr_Len = sizeof(client_Addr);
     sConnect = accept(server_socket, (SOCKADDR*)&client_Addr, &client_Addr_Len);
+
+    // if connect -> send message to client
     if(sConnect != INVALID_SOCKET){
-        cout<<"Got connection from "<<inet_ntoa(client_Addr.sin_addr);
+        cout<<"Got connection from "<<inet_ntoa(client_Addr.sin_addr)<<endl;
+        cout<<"等待玩家進入遊戲..";
+
+        char *sendbuf;
+        sendbuf = "Connect successfully to server！";
+        send(sConnect, sendbuf, (int)strlen(sendbuf), 0);
+
+        char message[100];
+        ZeroMemory(message, 100);
+        recv(sConnect,message,sizeof(message),0);
+        cout<<message;
+
+        Sleep(1000);
+        system("cls");
+        paint();
+
     }
 
     return 0;
